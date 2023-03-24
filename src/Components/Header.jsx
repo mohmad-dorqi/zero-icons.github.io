860e75c654106250f5bd548f537d28a242f66ca8
Peyman (ATH) Naderi <peymanath@gmail.com>
2023-03-24 20:08:29 +0430

    added logo to header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,11 @@
 import Image from "../Components/Image";
 const Header = props => {
 	return (
-		<div {...props}>
-			<p>Zero Icons</p>
-			<Image nameImage="zero-icons-white.svg" />
+		<div className="m-auto w-40 py-5" {...props}>
+			<div className="flex gap-5 items-center ">
+				<Image nameImage="zero-icons-white.svg" className="w-10" />
+				<p className="font-bold text-base lg:text-lg xl:text-xl ">Zero Icons</p>
+			</div>
 		</div>
 	);
 };
--- src/Components/Image.jsx
+++ src/Components/Image.jsx
@@ -1,16 +1,7 @@
-function Image({ className, nameImage, alt, title, width, height }) {
-	const ImageURL = new URL(`../assets/${nameImage}`, import.meta.url).href
+function Image(props) {
+	const ImageURL = new URL(`../assets/${props.nameImage}`, import.meta.url).href;
 
-	return (
-		<img
-			width={width}
-			height={height}
-			src={ImageURL}
-			alt={alt}
-			title={title}
-			className={className}
-		/>
-	);
+	return <img src={ImageURL} {...props} />;
 }
 
 export default Image;
