ddac913e72f86b8561c697a602238df4311facd5
Peyman (ATH) Naderi <peymanath@gmail.com>
2023-03-24 21:32:51 +0430

    added border color to icons

--- src/Components/ShowIcon.jsx
+++ src/Components/ShowIcon.jsx
@@ -20,23 +20,82 @@ const ShowIcon = () => {
 	};
 
 	const colors = [
-		"text-red-500",
-		"text-sky-500",
-		"text-blue-500",
-		"text-orange-500",
-		"text-amber-500",
-		"text-yellow-500",
-		"text-lime-500",
-		"text-emerald-500",
-		"text-green-500",
-		"text-teal-500",
-		"text-cyan-500",
-		"text-indigo-500",
-		"text-violet-500",
-		"text-purple-500",
-		"text-fuchsia-500",
-		"text-pink-500",
-		"text-rose-500",
+		{
+			text: "text-red-500",
+			border: "border-red-500",
+		},
+		{
+			text: "text-sky-500",
+			border: "border-sky-500",
+		},
+		{
+			text: "text-blue-500",
+			border: "border-blue-500",
+		},
+		{
+			text: "text-orange-500",
+			border: "border-orange-500",
+		},
+		{
+			text: "text-amber-500",
+			border: "border-amber-500",
+		},
+		{
+			text: "text-yellow-500",
+			border: "border-yellow-500",
+		},
+		{
+			text: "text-lime-500",
+			border: "border-lime-500",
+		},
+		{
+			text: "text-yellow-500",
+			border: "border-yellow-500",
+		},
+		{
+			text: "text-emerald-500",
+			border: "border-emerald-500",
+		},
+		{
+			text: "text-yellow-500",
+			border: "border-yellow-500",
+		},
+		{
+			text: "text-green-500",
+			border: "border-green-500",
+		},
+		{
+			text: "text-teal-500",
+			border: "border-teal-500",
+		},
+		{
+			text: "text-cyan-500",
+			border: "border-cyan-500",
+		},
+		{
+			text: "text-indigo-500",
+			border: "border-indigo-500",
+		},
+		{
+			text: "text-violet-500",
+			border: "border-violet-500",
+		},
+		{
+			text: "text-purple-500",
+			border: "border-purple-500",
+		},
+		{
+			text: "text-fuchsia-500",
+			border: "border-fuchsia-500",
+		},
+		{
+			text: "text-pink-500",
+			border: "border-pink-500",
+		},
+		{
+			text: "text-rose-500",
+			border: "border-rose-500",
+		},
 	];
 
 	return (
@@ -44,6 +103,9 @@ const ShowIcon = () => {
 			{keys &&
 				keys.map((iconName, index) => {
 					const IconComponent = ListIconOutline[iconName];
+
+					const randomNumber = Math.floor(Math.random() * 17);
+
 					return (
 						<CopyToClipboard
 							text={`<` + `${iconName} />`}
@@ -52,11 +114,11 @@ const ShowIcon = () => {
 							<li className="p-5 flex flex-col items-center gap-3">
 								<span
 									className={`border-2 ${
-										copied === iconName ? "border-red-700" : ""
+										copied === iconName ? colors[randomNumber].border : ""
 									} rounded-lg p-5 cursor-pointer`}>
 									<IconComponent
 										size="4.2rem"
-										className={colors[Math.floor(Math.random() * 17)]}
+										className={colors[randomNumber].text}
 									/>
 								</span>
 								<span className="font-bold text-xs">{iconName}</span>
