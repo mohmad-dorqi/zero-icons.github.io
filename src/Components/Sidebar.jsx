426fc4e2e4c55601f1296b44b6ad1d7191adf5f2
Peyman (ATH) Naderi <peymanath@gmail.com>
2023-03-25 13:23:44 +0430

    added new menus

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -3,29 +3,80 @@ import { NavLink } from "react-router-dom";
 const Sidebar = props => {
 	const menuSidebar = [
 		{
-			name: "Installation",
+			megaMenu: false,
+			megaMenuName: "Home",
+			megaMenuList: [],
+			name: "Home",
 			link: "/",
 		},
 		{
-			name: "Huge Icons",
-			link: "/hugeicons",
+			megaMenu: true,
+			megaMenuName: "Svg Converte",
+			megaMenuList: [
+				{
+					name: "Installation",
+					link: "/svgconverte-installation",
+				},
+				{
+					name: "Usage",
+					link: "/svgconverte",
+				},
+			],
+		},
+		{
+			megaMenu: true,
+			megaMenuName: "Huge Icons",
+			megaMenuList: [
+				{
+					name: "Installation",
+					link: "/hugeicons-installation",
+				},
+				{
+					name: "Icons List",
+					link: "/hugeicons",
+				},
+			],
 		},
 	];
 
 	return (
 		menuSidebar && (
-			<div {...props}>
-				<ul className="flex flex-col gap-5">
-					{menuSidebar.map(({ name, link }, index) => (
-						<NavLink
-							to={link}
-							className={({ isActive }) =>
-								isActive ? "bg-white text-primary rounded-md py-1 px-3 font-bold" : ""
-							}>
-							{name}
-						</NavLink>
-					))}
-				</ul>
+			<div className="w-full" {...props}>
+				<div className="flex flex-col gap-5">
+					{menuSidebar.map(
+						({ megaMenu, megaMenuName, megaMenuList, ...props }, index) =>
+							megaMenu ? (
+								<div key={index}>
+									<p className="border-b pb-3 mb-3">{megaMenuName}</p>
+
+									<div className="flex flex-col gap-5">
+										{megaMenuList &&
+											megaMenuList.map(({ name, link }) => (
+												<NavLink
+													to={link}
+													className={({ isActive }) =>
+														isActive
+															? "bg-white text-primary rounded-md py-1 px-3 font-bold"
+															: "bg-white text-secondary rounded-md py-1 px-3 font-bold"
+													}>
+													{name}
+												</NavLink>
+											))}
+									</div>
+								</div>
+							) : (
+								<NavLink
+									to={props.link}
+									className={({ isActive }) =>
+										isActive
+											? "bg-white text-primary rounded-md py-1 px-3 font-bold"
+											: "bg-white text-secondary rounded-md py-1 px-3 font-bold"
+									}>
+									{props.name}
+								</NavLink>
+							),
+					)}
+				</div>
 			</div>
 		)
 	);
